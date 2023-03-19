8c9c35a0beb23d6fc4469a5b43805b29585573e7
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2023-03-19 11:44:41 +0700

    link front-end to back-end

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000/api',
+    baseURL: 'http://127.0.0.1:8080/api',
 })
 
-export const getAllExam = () => api.get(`/history`)
-export const getExamByMethod = std_id => api.get(`/history/${std_id}`)
+export const getHistory = () => api.get(`/history`)
+export const getHistoryByID = std_id => api.get(`/history/${std_id}`)
 
 const apis = {
-    getAllExam,
-    getExamByMethod,
+    getHistory,
+    getHistoryByID,
 }
 
 export default apis
