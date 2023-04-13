1590cc5033b63408fd532c1f451ffe2b5ef75034
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2023-04-14 00:07:17 +0700

    add function check match data between bc and db

--- src/page/view_tran.js
+++ src/page/view_tran.js
@@ -11,11 +11,13 @@ import {
 
 export default function ViewTran(){
     const [txhash, setTxhash] = useState("");
+    const [detailDB, setDetail ] = useState("");
     const{ token: { colorBgContainer },} = theme.useToken();
 
     const OnViewTransaction = async() =>{
         const response = await ViewData_Transaction(txhash);
-        console.log(response)
+        return response
+        // console.log(response)
     }
 
     const onViewData = async() => {
@@ -23,11 +25,54 @@ export default function ViewTran(){
         console.log(response)
     }
 
-    const onGetDataDB = async(_id) =>{
+    const onGetProofDB = async(_id) =>{
+        const response = await api.getProofByID(_id);
+        return response.data.data
+    }
+
+    const onGetHistoryDB = async(_id) =>{
         const response = await api.getHistoryByID(_id);
         return response.data.data[0]
     }
 
+    const checkData = async() =>{
+        try{
+            var data_bc =  await OnViewTransaction();
+            data_bc = JSON.parse(JSON.stringify(data_bc))
+            console.log(JSON.parse(JSON.stringify(data_bc)))
+            const proof_db =  await onGetProofDB(data_bc.StdID);
+            console.log(proof_db)
+            const data_db = await onGetHistoryDB(data_bc.StdID);
+            console.log(data_db)
+
+            const pdf_db = JSON.stringify(proof_db.file_pdf);
+            const hash_pdf_db = window.web3.utils.sha3(pdf_db);
+
+            const all_data = JSON.stringify(data_db)
+            const hash_all_data = window.web3.utils.sha3(all_data);
+
+            if(data_bc.HashPaper === hash_pdf_db & data_bc.HashData === hash_all_data){
+                setDetail(<span>
+                    ✅{" "}
+                    found data
+                    <br/>
+                    <a download={`${data_bc.StdID}_transcript`} href={proof_db.file_pdf}>
+                        Download PDF
+                    </a>
+                    <br />
+                    {all_data}
+                    </span>
+                );
+            }
+            else if(data_bc.HashPaper !== hash_pdf_db || data_bc.HashData !== hash_all_data){
+                setDetail("data not match");
+            }
+        }catch(error){
+            setDetail("not found transaction");
+        }
+    }
+
+    
     return(
         <div className="site-layout-content" 
             style={{ textAlign: 'center' ,background:colorBgContainer}} >
@@ -53,16 +98,13 @@ export default function ViewTran(){
                 <Form.Item
                     wrapperCol={{ offset: 8, span: 16, }}
                 >
-                    <Button type="primary" htmlType="submit" onClick={OnViewTransaction}>
+                    <Button type="primary" htmlType="submit" onClick={checkData}>
                         view transaction
                     </Button>
-                    <br></br>
-                    <br></br>
-                    <Button type="primary" htmlType="submit" onClick={onViewData}>
-                        view data
-                    </Button>
                 </Form.Item>
-
+            {/* <Form.Item> */}
+                <p>{detailDB}</p>
+            {/* </Form.Item> */}
           </Form>
         </div>
     )
